823bdd93b3b6f02bcc40dfedd26117ba9c8c1505
Azamat Bormokov <azamatbormokov@gmail.com>
2015-10-29 21:56:46 +0600

    123

---
+++ Domain/Entities/VoterPartyRelation.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Domain.Entities
+{
+    public class VoterPartyRelation
+    {
+        public int Id { get; set; }
+        [Display(Name = "Избиратель")]
+        public int? VoterId { get; set; }
+        [Display(Name = "Партия")]
+        public int? PartyId { get; set; }
+    }
+}
