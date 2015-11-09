746617e1eaa9811f992c9e16bbe2cc2a818fcb9d
Azamat Bormokov <azamatbormokov@gmail.com>
2015-11-09 22:36:35 +0600

    Добавлен новый тип карты - более типизированный, с номерами домов. Планирование миграции данных из Гугл карты в новый фреймвор.

---
+++ VoterManager/Controllers/MonitorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VoterManager.Controllers
+{
+    public class MonitorController : Controller
+    {
+        // GET: Monitor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+    }
+}
